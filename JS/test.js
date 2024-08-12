b1f39d2c4c314192f341c9dc8836acec524114e0
imwkdrh <imwkdrh7@naver.com>
2024-08-12 14:59:44 +0900

    2024-08-12 14:59

---
+++ JS/test.js
@@ -0,0 +1,14 @@
+const king = {
+  name: "이성계",
+
+  tombName: "태조",
+
+  birth: "1335-11-04",
+
+  death: "1408-06-27",
+};
+
+const { birth, name, tombName, death } = king;
+
+console.log(`조선의 1대 국왕 ${tombName}의 이름은 ${name}이다.`);
+console.log(birth, death);
